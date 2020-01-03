3dc559f668051ea1e2fabf6aefb5529121cdfa24
McDusia <magdalno222@gmail.com>
2020-01-03 22:11:02 +0100

    Analyze of bow angle started

--- inference_new.py
+++ inference_new.py
@@ -5,6 +5,8 @@ import os.path			# from os.path import exists
 import shutil			# shutil.copy2
 import glob
 import argparse
+
+import imutils as imutils
 import numpy as np
 import cv2
 from model.losses import bce_dice_loss, dice_loss, dice_coeff
@@ -38,9 +40,9 @@ def load_imgs(im_names):
     for im_name in im_names:
         print(im_name)
         _img = io.imread(im_name)
-        io.imshow(_img)
-        io.show()
-        input("Press Enter to continue...")
+        #io.imshow(_img)
+        #io.show()
+        #input("Press Enter to continue...")
         _img = transform.resize(_img, im_shape,  mode='constant')
         _img = np.expand_dims(_img, -1)
         _X.append(_img)
@@ -264,15 +266,47 @@ if __name__ == '__main__':
         pr = remove_small_regions(pr, 0.005 * np.prod(im_shape))
         pr_out = img_as_ubyte(pr)
 
-        io.imshow(pr)
-        io.show()
-        input("Displaying leg bones... Press Enter to continue...")
+        # TODO Analyze the angle of bone
+        contours, _ = cv2.findContours(pr_out.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        backtorgb = cv2.cvtColor(pr_out, cv2.COLOR_GRAY2RGB)
+
+        # Finding extreme points
+        cnts = cv2.findContours(pr_out.copy(), cv2.RETR_EXTERNAL,
+                                cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        c = max(cnts, key=cv2.contourArea)
+        # determine the most extreme points along the contour
+        extLeft = tuple(c[c[:, :, 0].argmin()][0])
+        extRight = tuple(c[c[:, :, 0].argmax()][0])
+        extTop = tuple(c[c[:, :, 1].argmin()][0])
+        extBot = tuple(c[c[:, :, 1].argmax()][0])
+
+        # draw the outline of the object and extreme points
+        cv2.drawContours(backtorgb, [c], -1, (0, 255, 255), 2)
+        cv2.circle(backtorgb, extLeft, 8, (0, 0, 255), -1)
+        cv2.circle(backtorgb, extRight, 8, (0, 255, 0), -1)
+        cv2.circle(backtorgb, extTop, 8, (255, 0, 0), -1)
+        res = cv2.circle(backtorgb, extBot, 8, (255, 255, 0), -1)
+
+        # show the output image
+        cv2.imshow("Extreme points", res)
+        cv2.waitKey(0)
+
+        # TODO Refactor
+        # draw contours for first bone
+        x, y, w, h = cv2.boundingRect(contours[0])
+        test = cv2.rectangle(backtorgb, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        # draw contours for the second bone
+        x, y, w, h = cv2.boundingRect(contours[1])
+        test = cv2.rectangle(backtorgb, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        cv2.imshow('Leg bones with rectangle', test)
+        print("Press any key to continue...")
+        cv2.waitKey(0)
 
         copy_image_to_results(image, pr_opened)
         copy_mask_to_results(image_name_no_extension)
-
-        #_file_name_in = dataset_bow_legs_dir + '/' + image_name_no_extension + '_mask' + '.png'
-
 
         # ---------------------------------------------------------------
         # Conversion to a new size
