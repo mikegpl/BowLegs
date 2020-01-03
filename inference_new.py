20c82338c9d882374169e965c15e357b5fecf986
McDusia <magdalno222@gmail.com>
2020-01-03 18:15:10 +0100

    refactor of saving results

--- inference_new.py
+++ inference_new.py
@@ -20,24 +20,25 @@ os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"]=""
 os.environ["CUDA_VISIBLE_DEVICES"]='-1'
 
-
-#dataset_bow_legs_dir = 'uploads'
-#img_names = ['!002115_.png', '!002308_.png']
-#mask_names = ['!002115__mask.png', '!002308__mask.png']
-dataset_bow_legs_dir = 'dataset_bow-legs'
-img_names = ['mask_050/!002115_.png', 'mask_051/!002308_.png']
-mask_names = ['mask_050/!002115__mask.png', 'mask_051/!002308__mask.png']
+dir_img_mask = 'results/new/'
+dataset_bow_legs_dir = 'uploads'
+img_names = ['!002115_.png', '!002308_.png']
+mask_names = ['!002115__mask.png', '!002308__mask.png']
+# dataset_bow_legs_dir = 'dataset_bow-legs'
+# img_names = ['mask_050/!002115_.png', 'mask_051/!002308_.png']
+# mask_names = ['mask_050/!002115__mask.png', 'mask_051/!002308__mask.png']
+im_shape = (512, 256)
 
 
 # TODO refactor these fun
 
 
-def load_imgs(im_names, _im_shape):
+def load_imgs(im_names):
     _X = []
     for im_name in im_names:
         print(im_name)
         _img = io.imread(im_name)
-        _img = transform.resize(_img, _im_shape,  mode='constant')
+        _img = transform.resize(_img, im_shape,  mode='constant')
         _img = np.expand_dims(_img, -1)
         _X.append(_img)
     _X = np.array(_X)
@@ -46,11 +47,11 @@ def load_imgs(im_names, _im_shape):
     return _X
 
 
-def load_masks(im_names, _im_shape):
+def load_masks(im_names):
     y = []
     for im_name in im_names:
         _img = io.imread(im_name)
-        _img = transform.resize(_img, _im_shape,  mode='constant')
+        _img = transform.resize(_img, im_shape,  mode='constant')
         _img = np.expand_dims(_img, -1)
         y.append(_img)
     y = np.array(y)
@@ -83,8 +84,8 @@ def masked(img, gt, mask, alpha=1):
     rows, cols = img.shape
     color_mask = np.zeros((rows, cols, 3))
 
-    min_val = gt.min()	
-    max_val = gt.max()
+    # min_val = gt.min()
+    # max_val = gt.max()
     # print('min_val = {} max_val = {}\n'.format(min_val, max_val))
 
     # boundary = morphology.dilation(gt, morphology.disk(3)) - gt
@@ -105,11 +106,11 @@ def masked(img, gt, mask, alpha=1):
     return img_masked
 
 
-def remove_small_regions(img, size):
+def remove_small_regions(_img, size):
     """Morphologically removes small (less than size) connected regions of 0s or 1s."""
-    img = morphology.remove_small_objects(img, size)
-    img = morphology.remove_small_holes(img, size)
-    return img
+    _img = morphology.remove_small_objects(_img, size)
+    _img = morphology.remove_small_holes(_img, size)
+    return _img
 # -------------------------------------------------------------------
 
 
@@ -165,19 +166,46 @@ def load_weights():
     print("Weights loaded to model from the disc")
 
 
+def save_mean_results(_ious, _dices):
+    print('Mean IoU:{:.4f} Mean Dice:{:.4f}'.format(_ious.mean(), _dices.mean()))
+    with open("results/bow-legs_results.txt", "a", newline="\r\n") as f:
+        print('Mean IoU:{:.4f} Mean Dice:{:.4f}'.format(_ious.mean(), _dices.mean()), file=f)
+        print('\n', file=f)
+
+    with open("results/bow-legs_IoU_Dice.txt", "a", newline="\r\n") as f:
+        print('Mean IoU:{:.4f} Mean Dice:{:.4f}'.format(_ious.mean(), _dices.mean()), file=f)
+
+
+def copy_image_to_results(_image, _pr_opened):
+    if args.save_out_images:
+        if not os.path.exists(dir_img_mask):
+            os.makedirs(dir_img_mask)
+        cv2.imwrite(_image, _pr_opened)
+
+
+def copy_mask_to_results(_image_name_no_extension):
+    if args.save_out_images:
+        _file_name_in = dataset_bow_legs_dir + '/' + _image_name_no_extension + '_mask' + '.png'
+        _file_name_out = 'results/new/' + _image_name_no_extension + '_mask_manual' + '.png'
+        _img_exists = os.path.isfile(_file_name_in)
+        if not _img_exists:
+            print('{} does not exists\n'.format(_file_name_in))
+            sys.exit("Exit ...")
+        shutil.copy2(_file_name_in, _file_name_out)
+
+
 if __name__ == '__main__':
 
     args = parse_args()
-    im_shape = (512, 256)
 
     full_img_names = get_full_path_for_data(img_names)
     full_mask_names = get_full_path_for_data(mask_names)
-    X = load_imgs(full_img_names,  im_shape)
-    Y = load_masks(full_mask_names, im_shape)
+    X = load_imgs(full_img_names)
+    Y = load_masks(full_mask_names)
     print('Data loaded')
 
-    n_test = X.shape[0]
-    inp_shape = X[0].shape
+    number_of_test_data = X.shape[0]
+    input_shape = X[0].shape
     print('X.shape={} Y.shape={}'.format(X.shape, Y.shape))
 
     # Load model and trained weights
@@ -190,67 +218,42 @@ if __name__ == '__main__':
     model.compile(optimizer=RMSprop(lr=0.0001), loss=bce_dice_loss, metrics=[dice_coeff])	
     print("Model compiled")
 
-    ious = np.zeros(n_test)
-    dices = np.zeros(n_test)
-    mean_IoU = np.zeros(n_test)
+    ious = np.zeros(number_of_test_data)
+    dices = np.zeros(number_of_test_data)
+    mean_IoU = np.zeros(number_of_test_data)
 
-    i = 0
-    num_imgs = X.shape[0]
-    for ii in range(num_imgs):
-        xx_ = X[ii, :, :, :]
-        yy_ = Y[ii, :, :, :]
+    for image in img_names:
+        print(image)
+        image_name_no_extension = os.path.splitext(image)[0]
+        image_counter = 0
+
+        xx_ = X[image_counter, :, :, :]
+        yy_ = Y[image_counter, :, :, :]
         xx = xx_[None, ...]
         yy = yy_[None, ...]		
         img = exposure.rescale_intensity(np.squeeze(xx), out_range=(0, 1))
 
-        pred = UNet.predict(xx)[..., 0].reshape(inp_shape[:2])
-        mask = yy[..., 0].reshape(inp_shape[:2])
+        pred = UNet.predict(xx)[..., 0].reshape(input_shape[:2])
+        mask = yy[..., 0].reshape(input_shape[:2])
 
-        # Binarize masks
+        # Binary masks
         gt = mask > 0.5
         pr = pred > 0.5
 
         pr_bin = img_as_ubyte(pr)
-        pr_openned = morphology.opening(pr_bin)
+        pr_opened = morphology.opening(pr_bin)
 
-        # Remove regions smaller than 2% of the image
+        # Remove regions smaller than 0.5% of the image
         pr = remove_small_regions(pr, 0.005 * np.prod(im_shape))
-        # pr = remove_small_regions(pr, 0.02 * np.prod(im_shape))
-        pr_out = img_as_ubyte( pr)	
-
-        file_name = '!002308_.png'
-        print('file_name')
-        print(file_name)
-
-        if args.save_out_images:
-            #dir_img_mask = 'results/bow-legs_test/{}'.format(sub_dir_name)
-            dir_img_mask = 'results/bow-legs_test/'
-            if not os.path.exists(dir_img_mask):
-                os.makedirs(dir_img_mask)
-            img_name = file_name
-
-            cv2.imwrite(img_name, pr_openned)
-
-        file_name_no_ext = os.path.splitext(file_name)[0]
-        # ('file', '.ext')  --> os.path.splitext(file_name_no_ext)[0] ('file')
-        file_name_in = dataset_bow_legs_dir + '/' + file_name_no_ext + '_mask' + '.png'
-        # dataset_bow-legs/mask_001/img_0001_mask.png
-
-        if args.save_out_images:
-
-            file_name_out = 'results/bow-legs_test' + '/' + file_name_no_ext + '_mask_manual' + '.png'
-            # results/bow-legs_test/img_0006_mask_manual.png
-
-            img_exists = os.path.isfile(file_name_in)		
-            if not img_exists:
-                print('{} does not exists\n'.format(file_name_in))
-                sys.exit("Exit ...")
-
-            shutil.copy2(file_name_in, file_name_out)
+        pr_out = img_as_ubyte(pr)
+        
+        copy_image_to_results(image, pr_opened)
+        copy_mask_to_results(image_name_no_extension)
 
         # ---------------------------------------------------------------
         # Conversion to a new size
-        # im_name_x_ray_original_size_test = dataset_bow_legs_dir + '/' + 'x-ray_test/' + file_name			# data_bow-legs/x-ray/img_0001.png
+        # im_name_x_ray_original_size_test = dataset_bow_legs_dir + '/' + 'x-ray_test/' + file_name
+        ###  data_bow-legs/x-ray/img_0001.png
         # im_x_ray_original_size = cv2.imread(im_name_x_ray_original_size_test, cv2.IMREAD_GRAYSCALE)
         #
         # height, width = im_x_ray_original_size.shape[:2]							# height, width  -- original image size
@@ -293,26 +296,15 @@ if __name__ == '__main__':
         #     im_masked_4x = img_as_ubyte( im_masked_4x)
         #     io.imsave( im_name_masked_4x, im_masked_4x)
 
-        ious[i] = IoU(gt, pr)
-        dices[i] = Dice(gt, pr)
-        print('{}  {:.4f} {:.4f}'.format(img_names[ii], ious[i], dices[i]))
+        ious[image_counter] = IoU(gt, pr)
+        dices[image_counter] = Dice(gt, pr)
+        print('{}  {:.4f} {:.4f}'.format(img_names[image_counter], ious[image_counter], dices[image_counter]))
 
         with open("results/bow-legs_results.txt", "a", newline="\r\n") as f:
-            print('{}  {:.4f} {:.4f}'.format(img_names[ii], ious[i], dices[i]), file=f)
+            print('{}  {:.4f} {:.4f}'.format(img_names[image_counter], ious[image_counter], dices[image_counter]), file=f)
 
-        i += 1
-        if i == n_test:
-            break
+        image_counter += 1
 
-    print('Mean IoU:{:.4f} Mean Dice:{:.4f}'.format( ious.mean(), dices.mean() ))
-    with open("results/bow-legs_results.txt", "a", newline="\r\n") as f:
-        print('Mean IoU:{:.4f} Mean Dice:{:.4f}'.format(ious.mean(), dices.mean() ), file=f)
-        print('\n', file=f)
+    save_mean_results(ious, dices)
 
-    with open("results/bow-legs_IoU_Dice.txt", "a", newline="\r\n") as f:
-        print( 'Mean IoU:{:.4f} Mean Dice:{:.4f}'.format(ious.mean(), dices.mean() ), file=f)
-
-# print('\n')
-# print('Press any key to exit ')
-# cv2.waitKey(0)
 cv2.destroyAllWindows()
