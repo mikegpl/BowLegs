dc8c61869c13c7f7e65ef06628317e42bdc0706d
Michał Grabowski <mkg.grabowski@gmail.com>
2020-01-12 23:37:40 +0100

    Reformats application.py

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, flash, request, redirect, url_for
+
+from flask import Flask, flash, request, redirect
 from werkzeug.utils import secure_filename
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -11,32 +12,33 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 def allowed_file(filename):
-   return '.' in filename and \
-          filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route("/", methods=['GET', 'POST'])
 def upload_file():
-   if request.method == 'POST':
-      # check if the post request has the file part
-      if 'file' not in request.files or 'file2' not in request.files:
-         flash('No file part')
-         return redirect(request.url)
-      file = request.files['file']
-      file2 = request.files['file2']
-      # if user does not select file, browser also
-      # submit an empty part without filename
-      if file.filename == '' or file2.filename == '':
-         flash('No selected file')
-         return redirect(request.url)
-      if file and allowed_file(file.filename) and file2 and allowed_file(file2.filename):
-         filename = secure_filename('photo_' + file.filename)
-         filename2 = secure_filename('mask_' + file2.filename)
-         if not os.path.exists(UPLOAD_FOLDER):
-            os.makedirs(UPLOAD_FOLDER)
-         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-         file2.save(os.path.join(app.config['UPLOAD_FOLDER'], filename2))
-         return redirect(request.url)
-   return '''
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files or 'file2' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        file2 = request.files['file2']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '' or file2.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename) and file2 and allowed_file(file2.filename):
+            filename = secure_filename('photo_' + file.filename)
+            filename2 = secure_filename('mask_' + file2.filename)
+            if not os.path.exists(UPLOAD_FOLDER):
+                os.makedirs(UPLOAD_FOLDER)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            file2.save(os.path.join(app.config['UPLOAD_FOLDER'], filename2))
+            return redirect(request.url)
+    return '''
    <!doctype html>
    <title>Bones angle counter</title>
    <h1>Bones angle counter</h1>
@@ -49,7 +51,9 @@ def upload_file():
    </form>
    '''
 
+
 from flask import send_from_directory
+
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
